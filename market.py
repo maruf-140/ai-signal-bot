32457c64ad13385b6d061b9a604b7fd99b304bc3
maruf-140 <mdmarufk334@gmail.com>
2026-07-29 22:40:29 +0600

    Delete market.py

--- market.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def get_price(symbol="BTCUSDT"):
-    url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}"
-    r = requests.get(url, timeout=10)
-    r.raise_for_status()
-    return float(r.json()["price"])
